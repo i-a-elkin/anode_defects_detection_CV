ca71bea3c9f9ecafc0ea313f76b671d501a70844
Ivan Elkin <ivan.a.elkin@gmail.com>
2022-09-04 23:16:00 +0400

    Augmentation and new images

--- image_compression.py
+++
@@ -1,9 +0,0 @@
-def compression(array, coef):
-
-    new_list = [0] * (array.shape[1] // coef)
-    for i in range(array.shape[0] // coef):
-        new_list[i] = []
-        for j in range(array.shape[1] // coef):
-            compression = round(array[i*coef:i*coef+coef, j*coef:j*coef+coef].mean())
-            new_list[i].append(int(compression))
-    return np.array(new_list)
